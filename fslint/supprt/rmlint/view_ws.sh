45d361999e14c4e6c4bf924d8b287b980e63e2fc
Pádraig Brady <P@draigBrady.com>
2007-05-04 13:16:02 +0000

    Preliminary view "bad" whitespace support (using [g]vim
    git-svn-id: https://fslint.googlecode.com/svn/trunk@57 494acc53-a219-0410-adff-8f8ff343f446

---
+++ fslint/supprt/rmlint/view_ws.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#TODO: only use gvim for text files
+#else open items parent
+
+# View whitespace using [g]vim syntax highlighting
+# Pass -g option to use gvim, otherwise vim is used
+# Note one can pass - to read stdin
+
+if [ "$1" = "-g" ]; then
+    vim="gvim" #maybe -f to not fork?
+    shift
+else
+    vim="vim"
+fi
+
+if [ "$#" = "1" ]; then
+    if [ -d "$1" ]; then
+        gnome-open "$1" #TODO: what's free desktop equiv of this
+        exit
+    fi
+fi
+
+if ! $vim -h >/dev/null 2>&1; then
+    cat >/dev/null #to stop xargs warning
+    echo "Error: please install $vim to view whitespace" >&2
+    exit
+fi
+
+$vim -R \
++'highlight RedundantSpaces term=standout ctermbg=red guibg=red' \
++'match RedundantSpaces /\s\+$\| \+\ze\t/' \
++'set listchars=tab:>-,trail:.,extends:>' \
++'set list!' "$@"
