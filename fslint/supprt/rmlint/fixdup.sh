dbba3b5bf6947f61ad6195201fdd0df183527aac
Pádraig Brady <P@draigBrady.com>
2017-01-21 14:32:17 +0000

    support symlinking duplicates
    Fixes issue #42

--- fslint/supprt/rmlint/fixdup.sh
+++ fslint/supprt/rmlint/fixdup.sh
@@ -4,6 +4,7 @@
 
 suffix=.fixdup.$$
 
+symlink=0
 link=1
 dryRun=0
 if [ "$#" = "1" ]; then
@@ -14,6 +15,11 @@ if [ "$#" = "1" ]; then
         dryRun=1
     elif [ "$1" = "tmerge" ]; then
         dryRun=1
+    elif [ "$1" = "symlink" ]; then
+        symlink=1
+    elif [ "$1" = "tsymlink" ]; then
+        dryRun=1
+        symlink=1
     fi
 fi
 
@@ -25,7 +31,9 @@ while read file; do
     keepfile="$file"
     if [ "$dryRun" = "1" ]; then
         printf "\n\nkeeping:     $keepfile\n"
-        if [ "$link" = "1" ]; then
+        if [ "$symlink" = "1" ]; then
+            printf "symlinking: "
+        elif [ "$link" = "1" ]; then
             printf "hardlinking: "
         else
             printf "deleting: "
@@ -36,7 +44,10 @@ while read file; do
         printf "$file "
     else
         if [ "$link" = "1" ]; then
-            ln -f -b --suffix="$suffix" -- "$keepfile" "$file" 2>/dev/null ||
+            {
+             test "$symlink" = 0 &&
+             ln -f -b --suffix="$suffix" -- "$keepfile" "$file" 2>/dev/null;
+            } ||
             ln -sf -b --suffix="$suffix" -- "$keepfile" "$file"
             rm -f "$file$suffix"
         else
